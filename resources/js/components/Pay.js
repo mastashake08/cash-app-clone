deaf84f61ef2a8cacb3fe9bbd736407162e64b15
Jyrone Parker <jyrone.parker@gmail.com>
2019-05-27 22:56:35 -0400

    fix invariant error

--- resources/js/components/Pay.js
+++ resources/js/components/Pay.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import ReactDOM from 'react-dom';
-import Balance from './Balance';
 
 export default class Pay extends Component {
   constructor() {
